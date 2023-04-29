a53fabd409e5c9178fd6d0c6d550254753f1f6c6
MarianaKely <marianakely.r@gmail.com>
2023-04-28 23:56:09 -0300

    SignIn register route add

--- src/controllers/registerControllers.js
+++ src/controllers/registerControllers.js
@@ -3,6 +3,7 @@ import bcrypt from "bcrypt";
 import { registerSchema } from "../schemas/registerSchema.js";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv"
+import { v4 as uuidv4 } from "uuid";
 
 dotenv.config();
 
@@ -54,5 +55,72 @@ export async function signUp(req, res) {
   
       res.status(500).send(error.message);
       
+    }
+  }
+
+  export async function signIn(req, res) {
+
+    const { email, password } = req.body;
+  
+    const generateCode = uuidv4();
+  
+    try {
+  
+      const internalAnalysis = await db.collection("clientProfile").findOne({ email });
+  
+      if (!internalAnalysis) return res.status(400).send("Invalid username or password");
+  
+      const psswrd = bcrypt.compareSync(password, internalAnalysis.password);
+  
+      if (!psswrd) {
+  
+        return res.status(400).send("Invalid username or password");
+  
+      }
+  
+      const completeProfile = await db.collection("account").findOne({ _id: internalAnalysis._id });
+  
+      if (completeProfile) {
+  
+        await db.collection("account").updateOne(
+  
+          { _id: internalAnalysis._id },
+          {
+            $set: { token: generateCode },
+          }
+        );
+  
+      } else {
+  
+        const invalid = await db.collection("account").findOne({ _id: internalAnalysis._id });
+  
+        if (!invalid) {
+  
+          await db.collection("account").insertOne({ _id: internalAnalysis._id, token: generateCode });
+  
+        }
+      }
+  
+      const checkk = await db.collection("data").findOne({ _id: internalAnalysis._id });
+  
+      if (!checkk) {
+  
+        const mainUser = {
+  
+          _id: internalAnalysis._id,
+          name: internalAnalysis.name,
+          wallet: [],
+  
+        };
+        await db.collection("data").insertOne(mainUser);
+  
+      }
+  
+      return res.status(200).send(generateCode);
+  
+    } catch (error) {
+  
+      res.status(500).send(error.message);
+  
     }
   }
--- src/routes/registerRoute.js
+++ src/routes/registerRoute.js
@@ -1,9 +1,12 @@
  import { Router } from "express";
 import { signUp } from "../controllers/registerControllers.js";
+import { signIn } from "../controllers/registerControllers.js";
 
 
  const registerRoute = Router()
 
  registerRoute.post('/SignUp' , signUp);
+ registerRoute.post('/SignIn' , signIn);
+ 
 
  export default registerRoute;
