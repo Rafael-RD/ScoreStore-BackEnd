ecb46337b83c1b0fdb5986b1a91ef65482aa66d1
lucasmalacrida <lucasmd99@hotmail.com>
2023-05-02 17:56:44 -0300

    Feature - Post /cart

---
+++ src/controllers/order.controllers.js
@@ -0,0 +1,14 @@
+import { db } from "../database/database.connect.js";
+
+export async function postOrder(req, res){
+    const {cartList,token} = req.body;
+    try {
+        const user = await db.collection('account').findOne({token});
+        if (!user) return res.sendStatus(401)
+        await db.collection('orders').insertOne({cartList, _id: user._id});
+        return res.sendStatus(201);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500);
+    }
+}
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { getProducts } from "../controllers/products.controllers.js";
+import { postOrder } from "../controllers/order.controllers.js";
 
 const productsRouter=Router();
 
 productsRouter.get('/products', getProducts);
+products.post('/cart', postOrder);
 
 export default productsRouter;
